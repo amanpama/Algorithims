882203a08797b27627f9d289931e6bdf64e1ac21
Amanpreet Pama <amanpama@yahoo.com>
2022-09-20 09:58:03 -0700

    Algos !!

---
+++ pracSep20/pracSep20.js
@@ -0,0 +1,15 @@
+function fib(num) {
+    if (num === 1) {
+        return 0
+    }
+    else if (num === 2) {
+        return 1
+    }
+    else {
+
+        var i = fib(num - 1) + fib(num - 2)
+
+    }
+    return i
+}
+console.log(fib(30))
